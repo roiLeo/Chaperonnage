36f52d6a56f24d0f80ade8853e2aa8d4e1f685e8
Mathias-Form <mathias.e.babin@gmail.com>
2017-04-18 14:55:43 +0200

    Fin de design page de recherche

--- src/AppBundle/Resources/public/js/google_autocomplete.js
+++ src/AppBundle/Resources/public/js/google_autocomplete.js
@@ -1,14 +1,22 @@
-var placeSearch, autocompleteStart, autocompleteFinish;
-/*var componentForm = {
-    street_number: 'short_name',
-    route: 'long_name',
-    locality: 'long_name',
-    administrative_area_level_1: 'short_name',
-    country: 'long_name',
-    postal_code: 'short_name'
-};*/
+var autocompleteStart, autocompleteFinish;
+var markers = { start : null,
+                finish : null};
+var map;
 
 function initAutocomplete() {
+
+    var France = {lat: 46.7366065, lng: 1.402915};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: France
+    });
+    navigator.geolocation.getCurrentPosition(function(position) {
+        map.setCenter({lat: position.coords.latitude,
+                        lng: position.coords.longitude});
+    });
+
+
     // Create the autocomplete object, restricting the search to geographical
     // location types.
     autocompleteStart = new google.maps.places.Autocomplete(
@@ -18,38 +26,43 @@ function initAutocomplete() {
         /** @type {!HTMLInputElement} */(document.getElementById('finishAddress')),
         {types: ['geocode']});
 
-
     // When the user selects an address from the dropdown, event
-    autocompleteStart.addListener('place_changed', fillInAddress);
-    //autocompleteFinish.addListener('place_changed', fillInAddress);
+    autocompleteStart.addListener('place_changed', function(){fillInAddress('start', autocompleteStart)});
+    autocompleteFinish.addListener('place_changed', function(){fillInAddress('finish', autocompleteFinish)});
 }
 
-function fillInAddress() {
+function fillInAddress(addr, autocomplete) {
     // Get the place details from the autocomplete object.
-
-    var place = autocompleteStart.getPlace();
-    alert(place.address_components.toString());/*
-    for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        var val = place.address_components[i]];
-
-    }*/
-
-     /*
-    for (var component in componentForm) {
-        document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false;
-    }
+    var place = autocomplete.getPlace();
+    coord = {lat: place.geometry.location.lat(),
+                lng: place.geometry.location.lng()};
 
     // Get each component of the address from the place details
     // and fill the corresponding field on the form.
+    document.getElementById(addr+"Latitude").value = coord.lat;
+    document.getElementById(addr+"Longitude").value = coord.lng;
+
     for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        if (componentForm[addressType]) {
-            var val = place.address_components[i][componentForm[addressType]];
-            document.getElementById(addressType).value = val;
+        if(place.address_components[i].types[0]=="locality"){
+            document.getElementById(addr+"City").value = place.address_components[i].long_name;
         }
-    }*/
+        if(place.address_components[i].types[0]=="postal_code"){
+            document.getElementById(addr+"PostalCode").value = place.address_components[i].long_name;
+        }
+    }
+
+    //Showing the position on the map
+    if(markers[addr]){
+        markers[addr].setMap(null);
+    }
+
+    markers[addr] = new google.maps.Marker({
+    position: coord,
+    map: map,
+    title: addr,
+    });
+
+    map.setCenter(coord);
 }
 
 // Bias the autocomplete object to the user's geographical location,
